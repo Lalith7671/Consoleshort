7d234e6679e823631d178d8651e282294f027fee
lalith surya sai <lalith surya sai@LAPTOP-8VRVGF3U>
2025-05-23 11:22:06 +0530

    Add project files.

---
+++ Consoleshort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleshort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = 3276;
+            Console.WriteLine("value of a is {0}", a);
+            Console.WriteLine("short minimum value is {0}", short.MinValue);
+            Console.WriteLine("short maximum value is {0}", short.MaxValue);
+            Console.WriteLine("unsigned short minimum value is {0}", ushort.MinValue);
+            Console.WriteLine("unsigned short minimum value is {0}", ushort.MaxValue);
+            Console.WriteLine("size of short is {0}bytes", sizeof(short));
+            Console.WriteLine("base type of short is {0}", a.GetType());
+
+
+        }
+    }
+}
